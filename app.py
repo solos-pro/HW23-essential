d266796a049dffc88053220aa88c27fcb59f265a
solos-pro <sergey-solonichenko@rambler.ru>
2022-04-17 19:28:07 +0300

    1st step. Simple perform_query from Postman.

--- app.py
+++ app.py
@@ -1,20 +1,40 @@
 import os
 
-from flask import Flask
+from flask import Flask, request
+from werkzeug.exceptions import BadRequest
 
 app = Flask(__name__)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "data")
 
+def build_query():
+    pass
+
 
 @app.route("/perform_query")
 def perform_query():
+    try:
+        query = request.args["query"]
+        file_name = request.args["file_name"]
+    except KeyError:
+        raise BadRequest
+
+    file_path = os.path.join(DATA_DIR, file_name)
+    if not os.path.exists(file_path):
+        return BadRequest(description=f"{file_name} was not found")
+
+    with open(file_path) as fd:
+        print(fd)
+        res = build_query()
+        content = ''
+    return app.response_class(content, content_type="text/plain")
+
     # получить параметры query и file_name из request.args, при ошибке вернуть ошибку 400
     # проверить, что файла file_name существует в папке DATA_DIR, при ошибке вернуть ошибку 400
     # с помощью функционального программирования (функций filter, map), итераторов/генераторов сконструировать запрос
     # вернуть пользователю сформированный результат
-    return app.response_class('', content_type="text/plain")
+
 
 """from flask import Flask
 
